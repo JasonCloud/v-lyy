1a17782c1e0f712fb8ce10e4cb409fe0b455c485
jason <1210796178@qq.com>
2018-09-06 01:12:46 +0800

    优化picker组件

---
+++ src/components/Picker/Modal/Modal.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="Modal vlyy-modal-open">
+    <transition name="vlyy-mask">
+      <div class="vlyy-mask"
+           v-show="value"
+           :style="{zIndex: maskZIndex}"></div>
+    </transition>
+    <transition :name="dialogTransition">
+      <div class="vlyy-dialog"
+           v-show="value"
+           :style="styles">
+        <slot name="title">
+          <div class="vlyy-dialog-title">{{title}}</div>
+        </slot>
+        <slot name="content">
+          <div class="vlyy-dialog-body" >{{content}}</div>
+        </slot>
+        <slot name="foot">
+          <div class="vlyy-dialog-foot">
+            <button @click="sure" v-if="buttonOkText">{{buttonOkText}}</button>
+            <button @click="close">{{buttonCloseText}}</button>
+          </div>
+        </slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    value: Boolean,
+    title: {
+      type: String,
+      default: '我是标题'
+    },
+    content: {
+      type: String,
+      default: '我是内容'
+    },
+    buttonCloseText: {
+      type: String,
+      default: '关闭'
+    },
+    buttonOkText: {
+      type: String,
+      default: ''
+    },
+    dialogTransition: {
+      type: String,
+      default: 'vlyy-dialog'
+    },
+    maskZIndex: {
+      type: Number,
+      default: null
+    },
+    dialogZIndex: {
+      type: Number,
+      default: null
+    },
+    top: [Number, String],
+    width: [Number, String]
+  },
+  computed: {
+    styles () {
+      return {
+        top: this.top,
+        width: this.width,
+        zIndex: this.dialogZIndex
+      }
+    }
+  },
+  methods: {
+    sure () {
+      this.$emit('sure')
+      this.$emit('input', false)
+    },
+    close () {
+      this.$emit('close')
+      this.$emit('input', false)
+    }
+  }
+}
+</script>
+<style lang="less">
+  @import "../../assets/css/minix.less";
+  @import "../../assets/css/lyy.animated.less";
+  .vlyy-modal-open {
+    overflow: hidden;
+    position: fixed;
+    width: 100%
+  }
+  .vlyy-dialog-title{
+    padding: .5em 1em .5em;
+    .font-dpr(16px);
+    text-align: center;
+    font-family: "PingFangSC-Medium","Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+  }
+  .vlyy-dialog-body{
+    font-family: "PingFangSC-Regular", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+    padding: 0 1.6em .8em;
+    min-height: 40px;
+    .font-dpr(14px);
+    line-height: 1.3;
+    word-wrap: break-word;
+    word-break: break-all;
+    color: #999;
+  }
+  .vlyy-x-dialog-absolute{
+    position: absolute
+  }
+  .vlyy-mask {
+    background: rgba(0,0,0,.3);
+  }
+  .vlyy-mask{
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0
+  }
+  .vlyy-dialog {
+    position: fixed;
+    display: table;
+    z-index: 5000;
+    width: 80%;
+    max-width: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    background-color: #fff;
+    text-align: center;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  .vlyy-dialog-foot{
+    border-top: 1px solid #eeeeee;
+    .flex();
+    button{
+      background-color: #fff;
+      flex-grow: 1;
+      display: block;
+      .setFontSize(16px,#4e89ee);
+      padding: 14px 0;
+      border: none;
+      outline: none;
+    }
+    button + button {
+      border-left: 1px solid #d5d5d6;
+    }
+  }
+</style>
---
+++ src/components/Picker/Toast/Toast.vue
@@ -0,0 +1,164 @@
+<template>
+      <div class="vlyy-toast-wrap" :class="totastClass">
+        <div class="vlyy-mask"
+             v-show="show && isShowMask"></div>
+        <transition :name="Transition">
+          <div class="vlyy-toast" v-show="show">
+            {{text}}
+          </div>
+        </transition>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Toast',
+  data () {
+    return {
+      timer: null,
+      show: false
+    }
+  },
+  props: {
+    value: Boolean,
+    isShowMask: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: '我是 toast'
+    },
+    toastTransition: String,
+    position: String,
+    timeNum: {
+      type: Number,
+      default: 2000
+    }
+  },
+  created () {
+    if (this.value) {
+      this.show = true
+    }
+  },
+  watch: {
+    show (newVal) {
+      clearTimeout(this.timer)
+      if (newVal) {
+        this.timer = setTimeout(() => {
+          this.show = false
+          this.$emit('input', false)
+          this.close()
+          clearTimeout(this.timer)
+        }, this.timeNum)
+      }
+    },
+    value (newVal) {
+      this.show = newVal
+    }
+  },
+  computed: {
+    Transition () {
+      if (this.toastTransition) {
+        return this.toastTransition
+      } else if (this.position === 'bottom') {
+        return 'vlyy-slide-from-bottom'
+      } else if (this.position === 'top') {
+        return 'vlyy-slide-from-top'
+      } else if (this.position === 'middle') {
+        return 'vlyy-dialog'
+      } else {
+        return 'vlyy-fade'
+      }
+    },
+    totastClass () {
+      return {
+        'vlyy-toast-top': this.position === 'top',
+        'vlyy-toast-bottom': this.position === 'bottom',
+        'vlyy-toast-middle': this.position === 'middle',
+        'vlyy-toast-default': !this.position
+      }
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  @import "../../assets/css/minix.less";
+  @import "../../assets/css/lyy.animated.less";
+  .vlyy-toast-wrap {
+    position: fixed;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .vlyy-mask{
+      position: fixed;
+      z-index: 100;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      background-color: transparent;
+    }
+    .vlyy-toast {
+      .setFontSize(15px, #fff);
+      line-height: 150%;
+      background-color: rgba(0, 0, 0, .7);
+      max-width: 90%;
+      padding: 16px 26px;
+      border-radius: 10px;
+      z-index: 102;
+    }
+    .vlyy-slide-from-top-enter-active,
+    .vlyy-slide-from-top-leave-active,
+    .vlyy-slide-from-bottom-enter-active,
+    .vlyy-slide-from-bottom-leave-active{
+      transition: all 400ms cubic-bezier(.36,.66,.04,1);
+    }
+    .vlyy-slide-from-top-leave-to,
+    .vlyy-slide-from-bottom-leave-to,
+    .vlyy-slide-from-top-enter,
+    .vlyy-slide-from-bottom-enter{
+      opacity: 0;
+    }
+    .vlyy-slide-from-top-leave-to,
+    .vlyy-slide-from-top-enter{
+      transform:  translateY(-100%)!important;
+    }
+    .vlyy-slide-from-bottom-leave-to,
+    .vlyy-slide-from-bottom-enter{
+      transform:  translateY(100%)!important;
+    }
+  }
+  .vlyy-toast-top {
+    top: 0;
+    .center-hor;
+    .vlyy-toast{
+      opacity: 1;
+      transform: translateY(50%);
+    }
+  }
+  .vlyy-toast-bottom{
+    bottom: 0;
+    .center-hor;
+    .vlyy-toast{
+      opacity: 1;
+      transform: translateY(-50%);
+    }
+  }
+ .vlyy-toast-default{
+    .center-center;
+    .vlyy-toast{
+      opacity: 1;
+      transform: translateY(-50%);
+    }
+  }
+  .vlyy-toast-middle{
+    .center-center;
+  }
+</style>
